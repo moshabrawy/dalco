8a0b9fcb1b85cd0d26ddddbc2fae9cb1ef0f6e7b
Mo Shabrawy <mohamedshabrawy@affsquare.com>
2023-03-06 14:31:16 +0200

    Exceptions

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -36,19 +36,6 @@ class AboutController extends Controller
         $about->projects_info = !empty($projects_info)       ? $projects_info : $about->projects_info;
         $about->social        = !empty($social_media)        ? $social_media : $about->social_media;
         $about->save();
-        // $about->update([
-        //     'title_en'      => !empty($request->title_en)   ? $request->title_en : $about->title_en,
-        //     'title_ar'      => !empty($request->title_ar)   ? $request->title_ar : $about->title_ar,
-        //     'desc_en'       => !empty($request->desc_en)    ? $request->desc_en : $about->desc_en,
-        //     'desc_ar'       => !empty($request->desc_ar)    ? $request->desc_ar : $about->desc_ar,
-        //     'video'         => !empty($request->video)      ? $request->video : $about->video,
-        //     'address_en'    => !empty($request->address_en) ? $request->address_en : $about->address_en,
-        //     'address_ar'    => !empty($request->address_ar) ? $request->address_ar : $about->address_ar,
-        //     'email'         => !empty($request->email)      ? $request->email : $about->email,
-        //     'phone'         => !empty($request->phone)      ? $request->phone : $about->phone,
-        //     'projects_info' => !empty($projects_info)       ? $projects_info : $about->projects_info,
-        //     'social'        => !empty($social_media)        ? $social_media : $about->social_media,
-        // ]);
         notify()->success('You are awesome, your data was Updated successfully.');
         return redirect()->route('about.edit', ['about' => $about->id]);
     }
