e816cd32f56c37a255a70224f9268657c39d108b
moshabrawy <mo.shabrawydev@gmail.com>
2023-03-20 12:53:07 +0200

    js test

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
     "use strict";
 
-    if (window.location.pathname.includes('create')) {
-        document.querySelector('#create #description_en').value = '';
-        document.querySelector('#create #description_ar').value = '';
-    } else {
-        // console.log(window.location.pathname);
-    }
+    // if (window.location.pathname.includes('create')) {
+    //     document.querySelector('#create #description_en').value = '';
+    //     document.querySelector('#create #description_ar').value = '';
+    // } else {
+    //     // console.log(window.location.pathname);
+    // }
 
 });
